2dfa02e2b6eddee3f7893556e5c4d88e5e47d658
usr3 <usr3@usr1s-MacBook-Air.local>
2023-10-17 14:47:41 -0300

    Initial commit (frontend)

---
+++ frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,19 @@
+import BookSingleCard from "./BookSingleCard";
+
+import PropTypes from "prop-types";
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((item) => (
+        <BookSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  );
+};
+
+BooksCard.propTypes = {
+  books: PropTypes.array.isRequired,
+};
+
+export default BooksCard;
